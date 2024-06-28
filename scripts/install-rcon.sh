a0d54b52d310e942a496dafa5945eb73aeb978cd
Shellywell123 <44142139+Shellywell123@users.noreply.github.com>
2024-06-28 18:12:14 +0100

    Update install-rcon.sh

--- scripts/install-rcon.sh
+++ scripts/install-rcon.sh
@@ -1,3 +1,4 @@
+#! /usr/bin/bash
 wget https://github.com/gorcon/rcon-cli/releases/download/v0.10.3/rcon-0.10.3-amd64_linux.tar.gz
 tar xvzf rcon-0.10.3-amd64_linux.tar.gz
 sudo mv rcon-0.10.3-amd64_linux/rcon /usr/local/bin/
