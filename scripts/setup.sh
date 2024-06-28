cc91c2ee7c496c5a8f3bbb9a93dacf2699f7ba8e
Shellywell123 <44142139+Shellywell123@users.noreply.github.com>
2024-06-28 18:13:08 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,6 +18,13 @@ sudo mv wtf_*_linux_amd64/wtfutil /usr/local/bin/
 rm -rf wtf_*
 sudo chmod a+x /usr/local/bin/wtfutil
 
+# install rcon (needed for reporting factorio user counts)
+wget https://github.com/gorcon/rcon-cli/releases/download/v0.10.3/rcon-0.10.3-amd64_linux.tar.gz
+tar xvzf rcon-0.10.3-amd64_linux.tar.gz
+sudo mv rcon-0.10.3-amd64_linux/rcon /usr/local/bin/
+rm -rf rcon*
+sudo chmod a+x /usr/local/bin/rcon
+
 # make it callable from terminal
 sudo cp pi-dash /usr/local/bin
 
